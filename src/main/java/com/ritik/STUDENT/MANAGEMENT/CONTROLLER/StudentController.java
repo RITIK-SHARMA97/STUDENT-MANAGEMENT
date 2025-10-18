b0a080abd5eb25da4ee97f968a2324ad50b56053
RITIK-SHARMA97 <sharmaritik1297@gmail.com>
2025-10-18 20:12:55 +0530

    Added Student detail

--- src/main/java/com/ritik/STUDENT/MANAGEMENT/CONTROLLER/StudentController.java
+++ src/main/java/com/ritik/STUDENT/MANAGEMENT/CONTROLLER/StudentController.java
@@ -1,7 +1,15 @@
 package com.ritik.STUDENT.MANAGEMENT.CONTROLLER;
 
+import com.ritik.STUDENT.MANAGEMENT.DTO.StudentDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @GetMapping("/students")
+    public StudentDto getStudent(){
+        return new StudentDto(21L,"Ritik","sharma@gmail.com");
+    }
+
 }
