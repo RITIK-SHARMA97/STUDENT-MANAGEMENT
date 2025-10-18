1f3c1e3d5a3f5be8e0feba6abcc02b227c919ff7
RITIK-SHARMA97 <sharmaritik1297@gmail.com>
2025-10-18 19:54:58 +0530

    Initial commit

---
+++ src/main/java/com/ritik/STUDENT/MANAGEMENT/CONTROLLER/StudentController.java
@@ -0,0 +1,7 @@
+package com.ritik.STUDENT.MANAGEMENT.CONTROLLER;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+}
---
+++ src/test/java/com/ritik/STUDENT/MANAGEMENT/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritik.STUDENT.MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
